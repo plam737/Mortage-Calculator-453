b109f1784e0da9d213a4fda482383e76757ccf61
plam02 <peterkhanglam737@gmail.com>
2026-06-19 00:16:21 -0700

    Debugging Completed

--- mortgage_app/lib/main.dart
+++ mortgage_app/lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'mortgage_provider.dart';
 import 'main_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final mortgageProvider = MortgageProvider();
+  await mortgageProvider.loadFromPrefs();
+
+  runApp(
+    ChangeNotifierProvider.value(
+      value: mortgageProvider,
+      child: const MortgageApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MortgageApp extends StatelessWidget {
+  const MortgageApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MortgageV0',
       theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        useMaterial3: true,
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.indigo,
           foregroundColor: Colors.white,
